7c38d96f1dd4d0f6e6fbf61435de43891c4fff93
wenhaowuengeo <wwu@engeo.com>
2024-02-29 13:05:03 -0800

    update to append child action

--- src/index.js
+++ src/index.js
@@ -66,14 +66,6 @@ function runFunc () {
         });
       });
 
-
-      // if (status === "esriJobSucceeded"){
-      //   console.log("success, jobId:" + jobInfo.jobId);
-      //   var results = gp.getResultData(jobInfo.jobId, "out_feature_class", onTaskResultComplete);
-      //   console.log("past getResultsData");
-      //   console.log(results);
-      // };
-
     }).catch(function(e) {
       console.log("GP job failed", e);
     });
@@ -112,29 +104,15 @@ function emailFunc() {
   console.log("email button is clicked");
   document.getElementById("emailButtonID").style.color = 'blue';
 
-  //send alert
-  // alert("send email to Qiran");
-
   var emailDiv = document.createElement('div');
 
   if (htmlStr) {
     emailDiv.innerHTML = String(htmlStr);
   }
 
-  // emailDiv.innerHTML = String("&a href='mailto:aobertello@cbandg.com?&subject=CBG%20%20-%20Site Access Request Update for California Forever&body=Hi,%0D%0A Your site access request for the following APNs has been Updated%0D%0A0090-200-030 : Request Canceled%0D%0A0090-210-010 : Tenant Notified%0D%0A0090-220-170 : Tenant Notified' target='_blank'# Send Update to CBG : Angelo Obertello&/a#&br#&br#&a href='mailto:chemming@engeo.com?&subject=ENGEO%20%20-%20Site Access Request Update for California Forever&body=Hi,%0D%0A Your site access request for the following APNs has been Updated%0D%0A0042-010-070 : Tenant Approved%0D%0A0048-110-240 : Tenant Rejected%0D%0A0048-110-240 : Tenant Rejected%0D%0A0048-110-240 : Tenant Rejected' target='_blank'# Send Update to ENGEO : Chase Hemming&/a#&br#&br#&a href='mailto:arthur@sitelaburbanstudio.com?&subject=SITELAB/CMG%20%20-%20Site Access Request Update for California Forever&body=Hi,%0D%0A Your site access request for the following APNs has been Updated%0D%0A0042-100-150 : Tenant Notified%0D%0A0042-100-150 : Tenant Notified%0D%0A0042-100-140 : Tenant Notified%0D%0A0042-100-140 : Tenant Notified%0D%0A0048-100-390 : Delayed or Rejected Response Anticipated%0D%0A0048-100-390 : Delayed or Rejected Response Anticipated' target='_blank'# Send Update to SITELAB/CMG : Arthur Aperez Marseres&/a#&br#&br#&a href='mailto:RBrownsey@esassoc.com?&subject=ESA%20%20%20-%20Site Access Request Update for California Forever&body=Hi,%0D%0A Your site access request for the following APNs has been Updated%0D%0A0042-100-150 : Tenant Notified%0D%0A0042-100-150 : Tenant Notified%0D%0A0048-100-390 : Request Canceled%0D%0A0048-100-390 : Request Canceled%0D%0A0048-100-580 : Tenant Notified%0D%0A0177-100-050 : Delayed or Rejected Response Anticipated%0D%0A0177-100-050 : Delayed or Rejected Response Anticipated' target='_blank'# Send Update to ESA  : Rachel Brownsey&/a#&br#&br#&a href='mailto:Qliang@engeo.com?&subject=ENGEO%20%20-%20Site Access Request Update for California Forever&body=Hi,%0D%0A Your site access request for the following APNs has been Updated%0D%0A0042-010-150 : Tenant Approved%0D%0A0042-010-240 : Tenant Approved%0D%0A0042-010-280 : Tenant Approved%0D%0A0042-010-290 : Tenant Approved%0D%0A0042-010-360 : Tenant Approved%0D%0A0042-010-370 : Tenant Approved%0D%0A0042-010-560 : Tenant Approved%0D%0A0042-010-570 : Tenant Approved%0D%0A0042-010-590 : Tenant Approved' target='_blank'# Send Update to ENGEO : Qiran Liang&/a#&br#&br#")
-  //     .replace(/&/g,"<")
-  //     .replace(/#/g,">");
-
-  // var domBody = document.getElementByTagName('body')[0];
-  // domBody.appendChild('<br><>');    
   document.getElementsByTagName('body')[0].appendChild(document.createElement('br'))
-  
   document.getElementsByTagName('body')[0].appendChild(emailDiv);
 
-
 };
-
-// document.getElementById("runButtonID").onclick = runFunc();
-
 
 
